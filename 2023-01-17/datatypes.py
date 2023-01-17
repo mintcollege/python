bd7cf70d9fbf690d9e495af8327c70c1acfb4bc9
enchance <enchance@gmail.com>
2023-01-17 12:01:32 +0800

    welcome to python

---
+++ 2023-01-17/datatypes.py
@@ -0,0 +1,43 @@
+# Python is a dynamically-typed language
+# It knows the datatype you are assigning to the variable
+
+# str
+name = 'Bianca'
+
+# int
+age = 24
+
+# float
+rating = 3.5
+
+# bool
+has_paid = True
+has_car = False
+
+# list: always in the order you wrote it in
+scores = [100, 96, 99, 85]
+somedata = [5.5, 'green', True, 12]
+
+
+def show_hello_message():
+    # Prints hello world 3 times
+    message = 'Hello World'
+    print(message)
+    print(message)
+    print(message)
+    
+# show_hello_message()
+
+def christmas_tree():
+    # prints a tree
+    print('*')
+    print('**')
+    print('***')
+    print('****')
+    print('*****')
+    print('**')
+    print('*')
+    
+christmas_tree()
+christmas_tree()
+
---
+++ 2023-01-17/functions.py
@@ -0,0 +1,14 @@
+def show_hello_message():
+    # Prints hello world 3 times
+    print('hello world')
+    print('hello world')
+    print('hello world')
+
+show_hello_message()
+show_hello_message()
+
+def get_message():
+    # Need to write this function soon
+    # Get the message from the email
+    pass
+
